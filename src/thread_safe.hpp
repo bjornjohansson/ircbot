162429da89bb22d07878a4b973edadcbc8d930b2
Bjorn Johansson <bjorn@fiskbil.org>
2009-03-01 13:00:33 +0000

    Removed debug output

--- src/thread_safe.hpp
+++ src/thread_safe.hpp
@@ -11,18 +11,14 @@ public:
     ~thread_safe() { boost::upgrade_lock<boost::shared_mutex> lock(mutex_); }
     thread_safe<T>& operator=(const T& rhs)
     {
-	std::cout<<"thread_safe::operator="<<std::endl;
 	boost::upgrade_lock<boost::shared_mutex> lock(mutex_);
-	std::cout<<"thread_safe::operator=("<<rhs<<")"<<std::endl;
 	value_ = rhs;
 	return *this;
     }
 
     T operator*() const
     {
-	std::cout<<"thread_safe::operator*"<<std::endl;
 	boost::shared_lock<boost::shared_mutex> lock(mutex_);
-	std::cout<<"thread_safe::operator* returns "<<value_<<std::endl;
 	return value_;
     }
 
