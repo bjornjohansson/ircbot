d7616b0a616a5ff64584ae1510d3018db2f16be0
bjorn <bjorn@fiskbil.org>
2010-07-16 19:04:52 +0200

    Added a missing include and made include paths for icuwrap relative.

--- custom.py
+++ custom.py
@@ -1,5 +1,5 @@
 CPPFLAGS = '`xml2-config --cflags` -Wall -pedantic-errors -I/usr/include/lua5.1'
 LINKFLAGS = '`xml2-config --libs` `icu-config --ldflags`'
 CPPDEFINES = ['LUA_EXTERN']
-CPPPATH = ['/home/bjorn/workspace/icuwrap/src']
-LIBPATH = ['/home/bjorn/workspace/icuwrap/build/debug']
+CPPPATH = ['../../../icuwrap/src']
+LIBPATH = ['../../../icuwrap/build/debug']
--- src/irc/channel.hpp
+++ src/irc/channel.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <unicode/unistr.h>
+#include <string>
 
 class Server;
 
