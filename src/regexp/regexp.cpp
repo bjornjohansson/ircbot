a254a2aede4bb8087f21dfa8f94693a8455e9f3b
bjorn <bjorn@fiskbil.org>
2011-12-03 17:20:57 +0100

    Added \@ as a symbol for replacing matches, matches are escaped so that they are usiable within bash quotes

--- src/regexp/regexp.cpp
+++ src/regexp/regexp.cpp
@@ -6,8 +6,11 @@
 #include <iostream>
 
 #include <boost/algorithm/string/replace.hpp>
+#include <boost/function.hpp>
 
 #include <converter.hpp>
+
+typedef std::basic_string<uint16_t> ustring;
 
 const boost::regex::flag_type REGEX_FLAGS = boost::regex::extended
 		| boost::regex::icase;
@@ -32,6 +35,58 @@ RegExp::RegExp(const UnicodeString& regExp, const UnicodeString& reply,
 	}
 }
 
+void ReplaceIndex(UnicodeString& result,
+                  const UnicodeString& index,
+                  const ustring& match,
+                  boost::function<ustring (const ustring&)> modifier
+                  = boost::function<ustring (const ustring&)>())
+{
+	for (int32_t pos = result.indexOf(index);
+	     pos != -1;
+	     pos = result.indexOf(index, pos + 1))
+	{
+		if (pos == 0 || result[pos - 1] != '\\')
+		{
+			ustring replacement = match;
+			if (modifier)
+			{
+				replacement = modifier(replacement);
+			}
+			result.replace(pos,
+			               index.length(),
+			               reinterpret_cast<const uint16_t*>(replacement.c_str()));
+			
+			pos += replacement.size() - 1;
+		}
+		else if (pos > 0 && result[pos - 1] == '\\')
+		{
+			result.remove(pos - 1, 1);
+		}
+	}
+}
+
+void EscapeCharacter(ustring& input, uint16_t character)
+{
+	size_t pos = 0;
+	while ((pos=input.find(character, pos)) != ustring::npos)
+	{
+		input.insert(pos, 1, L'\\');
+		pos += 2;
+	}
+	
+}
+
+ustring QuoteBashString(const ustring& input)
+{
+	ustring result = input;
+	EscapeCharacter(result, L'\\');
+	EscapeCharacter(result, L'"');
+	EscapeCharacter(result, L'$');
+	EscapeCharacter(result, L'`');
+	
+	return result;
+}
+
 UnicodeString RegExp::FindMatchAndReply(const UnicodeString& message) const
 {
 	UnicodeString result;
@@ -44,23 +99,14 @@ UnicodeString RegExp::FindMatchAndReply(const UnicodeString& message) const
 			result = reply_;
 			for (unsigned int i = 0; i < matches.size(); ++i)
 			{
-				std::stringstream ss;
-				ss << '\\' << i;
-				UnicodeString index = AsUnicode(ss.str());
+				std::stringstream unquoted, quoted;
+				unquoted << '\\' << i;
+				quoted << '@' << i;
+				UnicodeString unquotedIndex = AsUnicode(unquoted.str());
+				UnicodeString quotedIndex = AsUnicode(quoted.str());
 
-				for (int32_t pos = result.indexOf(index);
-				     pos != -1; pos = result.indexOf(index, pos + 1))
-				{
-					if (pos == 0 || result[pos - 1] != '\\')
-					{
-						result.replace(pos, index.length(), matches[i].str().c_str());
-						pos += matches[i].str().size() - 1;
-					}
-					else if (pos > 0 && result[pos - 1] == '\\')
-					{
-						result.remove(pos - 1, 1);
-					}
-				}
+				ReplaceIndex(result, unquotedIndex, matches[i].str());
+				ReplaceIndex(result, quotedIndex, matches[i].str(), &QuoteBashString);
 			}
 		}
 	}
